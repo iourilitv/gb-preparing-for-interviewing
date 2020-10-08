260db1cbbd44b1f4fd7ab69c740a42fd456ea0b9
iourilitv <iourilitv1965@gmail.com>
2020-10-08 12:34:38 +0300

    Completed the task 1.

---
+++ src/main/java/ru/geekbrains/lesson1/hw/task1/PersonBuilder.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.lesson1.hw.task1;
+
+import java.util.logging.Logger;
+
+public class PersonBuilder {
+    Logger log = Logger.getLogger(this.getClass().getName());
+
+    public Person createNewPerson(String firstName, String lastName, String middleName, int age, String gender,
+                                  String country, String address, String phone) {
+        Person person = Person.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .middleName(middleName)
+                .age(age)
+                .gender(gender)
+                .country(country)
+                .address(address)
+                .phone(phone)
+                .build();
+        log.info("*** Created a new Person: " + person);
+        return person;
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson1/hw/task1/Task1.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.lesson1.hw.task1;
+
+/**
+ * Hw for lesson 1.
+ * @Author Litvinenko Yuriy
+ * 1. Создать builder для класса Person со следующими полями:
+ *    String firstName, String lastName, String middleName, String country, String address, String phone,
+ *    int age, String gender.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        PersonBuilder personBuilder = new PersonBuilder();
+        Person person = personBuilder.createNewPerson("Yuriy", "Litvinenko", "Semenovich", 55,
+                Genders.Male.name(), "Russia", "Senina, 44", "+7 999 999-99-99");
+    }
+}
