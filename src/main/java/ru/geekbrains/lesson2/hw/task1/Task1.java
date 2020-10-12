c102d11c12666d9a4f036e07f16dd2b3c4466b37
iourilitv <iourilitv1965@gmail.com>
2020-10-12 14:08:53 +0300

    Completed the task2 of lesson 2.

---
+++ src/main/java/ru/geekbrains/lesson2/hw/task1/Task1.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson2.hw.task1;
+
+import java.util.LinkedList;
+
+/**
+ * Hw for lesson 2.
+ * @Author Litvinenko Yuriy
+ * 1. Реализовать основные методы связанного списка.
+ */
+public class Task1 {
+//    LinkedList
+}
